afc5402766949a93a59721a5d3cff04fdc3210c1
YJbonobono <1004anny1@naver.com>
2021-11-21 22:23:25 +0900

    modified

--- classify.py
+++ classify.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import re
+import numpy
 
 m = ['ISTJ', 'ISFJ', 'INFJ', 'INTJ', 'ISTP', 'ISFP', 'INFP','INTP', 'ESTP', 'ESFP', 'ENFP', 'ENTP', 'ESTJ', 'ESFJ', 'ENFJ', 'ENTJ']
 postdata = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
@@ -13,22 +14,21 @@ def getFile():
     return data
 d = getFile()
 
-# Classify MBTI Types
+# Classify MBTI Types, 3-dimension array
 for (t,p) in d:
     loc = m.index(t)
     postdata[loc].append(p)
 
-# Join Total posts of each MBTI Types
+# Join Total posts of each MBTI Types, 2-dimension array
 tempdata = []
 for item in postdata:
     tempdata.append('|||'.join(item))
 # print(len(tempdata))
 
-newdata = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
-i=0
+newdata = []
 for text in tempdata:
     # URL
-    text = re.sub('(http|ftp|https|uhttp|$uhttp)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', ' ', text)
+    text = re.sub('(http|ftp|https|uhttp|$uhttp)://(?:[-\w.]|(?=:%[\da-fA-F]{2}))+', ' ', text)
     text = re.sub('http://([\d\w\./])+/[\d\w\./]+ ', ' ', text)
     text = re.sub('http://([\d\w\./])+/[\d\w\./]+.', ' ', text)
     text = re.sub('http : \*\*([\d;])+;TOOLONG', ' ', text)
@@ -46,29 +46,48 @@ for text in tempdata:
     # Delete blank entry
     t = list(map(lambda s: s.strip(), text_split))
     t = list(filter(None, t))
-    print(len(t))
-    newdata[i] = t
-    i +=1
-# print(len(newdata))
-# print(len(newdata[0]))
-
+    # print(len(t))
+    newdata.append(t)
 
 # WriteFile
-data = {'ISTJ':newdata[0],
-    'ISFJ':newdata[1],
-    'INFJ':newdata[2],
-    'INTJ':newdata[3],
-    'ISTP':newdata[4],
-    'ISFP':newdata[5],
-    'INFP':newdata[6],
-    'INTP':newdata[7],
-    'ESTP':newdata[8],
-    'ESFP':newdata[9],
-    'ENFP':newdata[10],
-    'ENTP':newdata[11],
-    'ESTJ':newdata[12],
-    'ESFJ':newdata[13],
-    'ENFJ':newdata[14],
-    'ENTJ':newdata[15]}
-data_df = pd.DataFrame(data)
-data_df.to_csv('./data-clean.csv', sep=',')
+# data = {
+#     'ISTJ': [newdata[0]],
+#     'ISFJ': [newdata[1]],
+#     'INFJ': [newdata[2]],
+#     'INTJ': [newdata[3]],
+#     'ISTP': [newdata[4]],
+#     'ISFP': [newdata[5]],
+#     'INFP': [newdata[6]],
+#     'INTP': [newdata[7]],
+#     'ESTP': [newdata[8]],
+#     'ESFP': [newdata[9]],
+#     'ENFP': [newdata[10]],
+#     'ENTP': [newdata[11]],
+#     'ESTJ': [newdata[12]],
+#     'ESFJ': [newdata[13]],
+#     'ENFJ': [newdata[14]],
+#     'ENTJ': [newdata[15]]
+# }
+data = {
+    'ISTJ': newdata[0],
+    'ISFJ': newdata[1],
+    'INFJ': newdata[2],
+    'INTJ': newdata[3],
+    'ISTP': newdata[4],
+    'ISFP': newdata[5],
+    'INFP': newdata[6],
+    'INTP': newdata[7],
+    'ESTP': newdata[8],
+    'ESFP': newdata[9],
+    'ENFP': newdata[10],
+    'ENTP': newdata[11],
+    'ESTJ': newdata[12],
+    'ESFJ': newdata[13],
+    'ENFJ': newdata[14],
+    'ENTJ': newdata[15]
+}
+
+dd = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in data.items() ]))
+# data_df = pd.DataFrame.from_dict(data)
+# data_fin = data_df.transpose()
+dd.to_csv('./data-clean2.csv', sep=',')
